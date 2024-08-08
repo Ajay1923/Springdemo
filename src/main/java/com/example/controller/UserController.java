a696c8017924804d9b06efc22b253254a5dc5832
AJAY KUMAR C <100348767+Ajay1923@users.noreply.github.com>
2024-08-08 18:34:23 +0530

    Update UserController.java

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -5,14 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.css.UserCSS;
+import com.example.dao.UserDAO;
 import com.example.model.User;
 
 @RestController
 public class UserController {
 
 	@Autowired
-	UserCSS usercss;
+	UserDAO userdao;
 	
 	@RequestMapping("index")
 	public String user()
@@ -22,15 +22,14 @@ public class UserController {
 	@RequestMapping("addUser")
 	public String user(User user)
 	{
-		usercss.save(user);
+		userdao.save(user);
 		return "index.jsp";
 	}
 	@RequestMapping("getUser")
 	public ModelAndView getuser(@RequestParm int id)
 	{
 		ModelAndView mav = new ModelAndView("showUser.jsp");
-		User user = usercss.findById(id).orElse(new User());
-
+		User user = userdao.findById(id).orElse(new User());
 		mav.addObject(user);
 		return mav;
 	}
@@ -38,17 +37,17 @@ public class UserController {
 	public ModelAndView deleteUser(@RequestParm int id)
 	{
 		ModelAndView mav = new ModelAndView("deleteUser.jsp");
-		User user = usercss.findById(id).orElse(new User());
-        Usercss.deleteById(id);
+		User user = userdao.findById(id).orElse(new User());
+                Userdao.deleteById(id);
 		mav.addObject(user);
 		return mav;
 	}
-    @RequestMapping("updateUser")
+        @RequestMapping("updateUser")
 	public ModelAndView updateUser(User user)
 	{
 		ModelAndView mav = new ModelAndView("updateUser.jsp");
-	    user = usercss.findById(user.getId()).orElse(new User());
-        Usercss.deleteById(user.getIdid);
+	        user = userdao.findById(user.getId()).orElse(new User());
+                Userdao.deleteById(user.getIdid);
 		mav.addObject(user);
 		return mav;
 	}
