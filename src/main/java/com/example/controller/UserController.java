e5ee48fe4b13daa2fc8fa80dcd674fcbd2c63cee
AJAY KUMAR C <100348767+Ajay1923@users.noreply.github.com>
2024-08-09 12:59:38 +0530

    Update UserController.java

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.dao.UserDAO;
 import com.example.model.User;
